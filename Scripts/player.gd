9662170948aa374df89d34e696e128a67b56ff47
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-03 05:48:46 +0500

    Adding animations to player according to direction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 160.0
+const JUMP_VELOCITY = -380.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +15,20 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
+	if direction < 0 or direction > 0:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("idle")
 
 	move_and_slide()
