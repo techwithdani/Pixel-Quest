cb89ba082c562cf50984471bfa01dcbf02a8d57f
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-03 12:34:00 +0500

    updating player script and player scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 220.0
-const JUMP_VELOCITY = -380.0
+const JUMP_VELOCITY = -300
+var jump_count = 0
+var max_jump_count = 2
 @onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,11 +15,15 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		
+	if is_on_floor():
+		jump_count = 0
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count += 1
+	
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
@@ -31,7 +37,6 @@ func _physics_process(delta):
 	elif direction > 0:
 		animated_sprite.flip_h = false
 
-
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -39,6 +44,6 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-
-
+	
+	
 	move_and_slide()
