45fc0eaf1ae4f4b1271b0d6437822d2f1ce1e9f8
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-04 20:42:36 +0500

    Created Enemy and updated other scenes and scripts

---
+++ Scripts/rhino.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED = 160.0
+var direction = -1
+@onready var ray_cast_right = $RayCast2DRight
+@onready var ray_cast_left = $RayCast2DLeft
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = true
+		
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = false
+		
+	position.x += SPEED * direction * delta
