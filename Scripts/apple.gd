6095884685ecb056fac9dc7a1ca70d71b6624e5b
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-04 08:52:44 +0500

    Updated scenes and scripts

--- Scripts/apple.gd
+++ Scripts/apple.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 func _on_body_entered(body):
-	queue_free()
+	animated_sprite.play("collected")
+	if animated_sprite.animation_changed:
+		queue_free()
--- Scripts/banana.gd
+++ Scripts/banana.gd
@@ -1,8 +1,5 @@
 extends Area2D
 
 
-@onready var animated_sprite = $AnimatedSprite2D
-
-
 func _on_body_entered(body):
 	queue_free()
