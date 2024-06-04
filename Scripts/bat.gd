8655cc4599e399ff7d60f121c144ca0308aa99d0
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-05 04:23:37 +0500

    Updated bat.tscn and other scenes

--- Scripts/bat.gd
+++ Scripts/bat.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 const SPEED = 120
 var is_player_chase = false
 var player = null
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	if is_player_chase:
 		velocity = (player.get_global_position() - position).normalized() * SPEED * delta
+		animated_sprite.play("fly")
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 00.7)
 	move_and_collide(velocity)
