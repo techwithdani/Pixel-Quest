2a2ef5ad36e6985eee2bb6d7bf96224b88e391c3
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-04 18:31:17 +0500

    Updated scripts

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 220.0
 const JUMP_VELOCITY = -300.0
 var jump_count = 0
@@ -9,7 +8,6 @@ var max_jump_count = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,14 +22,12 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
 	
-	
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
 	
 	if direction < 0:
 		animated_sprite.flip_h = true
@@ -45,6 +41,5 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	
 	
 	move_and_slide()
