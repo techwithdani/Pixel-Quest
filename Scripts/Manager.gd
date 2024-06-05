8bcea7558293efa7723aa9aa213d116beca5f2bc
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-05 06:52:51 +0500

    labels and score added

--- Scripts/Manager.gd
+++ Scripts/Manager.gd
@@ -1,17 +1,8 @@
-extends Node2D
+extends Node
 
-var apple_score_point = 0
-var banana_score_point = 0
-@onready var apples = $"../Apples"
-@onready var bananas = $"../Bananas"
-@onready var apple_score = $AppleScore
-@onready var banana_score = $BananaScore
-
+var points = 0
+@onready var score = $Score
 
 func add_score():
-	if apples:
-		apple_score_point += 1
-		apple_score.text = str(apple_score_point)
-	if bananas:
-		banana_score_point += 1
-		banana_score.text = str(banana_score_point)
+	points += 1
+	score.text = ": " + str(points)
