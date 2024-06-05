9b303c16f73804632414a2b531553e55be560990
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-06-05 07:37:57 +0500

    Game Updated

--- Scripts/Manager.gd
+++ Scripts/Manager.gd
@@ -2,7 +2,11 @@ extends Node
 
 var points = 0
 @onready var score = $Score
+@onready var you_died = $YouDied
 
 func add_score():
 	points += 1
 	score.text = ": " + str(points)
+
+func player_died():
+	you_died.text = "YOU DIED!"
--- Scripts/death_zone.gd
+++ Scripts/death_zone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	Engine.time_scale = 0.5
+	Engine.time_scale = 0.3
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
